822873ceb610977e8ac5a492c5dd0bbea0ad260c
BeluPuentes <belu.puentes@hotmail.com>
2023-02-28 13:07:08 +0000

    Turning down X-Ray and CloudWatch Logs

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -3,7 +3,7 @@ from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
     #X-Ray
-    segment = xray_recorder.begin_segment('user_activities')
+    #segment = xray_recorder.begin_segment('user_activities')
 
     model = {
       'errors': None,
@@ -28,9 +28,9 @@ class UserActivities:
 
     subsegment = xray_recorder.begin_subsegment('mock-data')
     #X-Ray
-    dict = {
-      "now" : now.isoformat(),
-      "results-size" : len(model['data'])
-    }
-    subsegment.put_metadata('key', dict, 'namespace')
+    #dict = {
+    #  "now" : now.isoformat(),
+    #  "results-size" : len(model['data'])
+    #}
+    #subsegment.put_metadata('key', dict, 'namespace')
     return model
